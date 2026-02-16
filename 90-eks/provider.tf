8e56d2fdc3c5bd84a7f73b225b94cd79b6a5b7c8
pramod <peruripramoddevops@gmail.com>
2026-02-16 18:32:24 +0530

    eks_tf

---
+++ 90-eks/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 6.28"
+    }
+  }
+
+  backend "s3" {
+    bucket = "pramod-daws"
+    key    = "roboshop-dev-eks"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
---
+++ 90-eks/variables.tf
@@ -0,0 +1,35 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z05714843SO1YQWMF1N5N"
+}
+
+variable "domain_name" {
+    default = "pramod.shop"
+}
+
+variable "eks_version" {
+
+}
+
+variable "eks_nodegroup_blue_version" {
+
+}
+
+variable "eks_nodegroup_green_version" {
+
+}
+
+variable enable_blue {
+
+}
+
+variable enable_green {
+    
+}
